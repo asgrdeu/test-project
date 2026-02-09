1f7c996a84bc435fe165771c26928f602640b562
asgrdeu <asgrdeu@gmail.com>
2026-02-09 15:41:28 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="p-8">
+    <div class="flex flex-row justify-start items-center gap-4">
+      <h1 class="text-xl font-semibold">Учетные записи</h1>
+      <Button variant="outline" size="icon-lg">
+        <Plus />
+      </Button>
+    </div>
+
+
+  </div>
+</template>
+
+<style scoped></style>
